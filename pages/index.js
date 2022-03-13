62f9312c77768aa2e534b709066359ebeafbeb73
Alex-Reyne <creativereyne@gmail.com>
2022-03-12 19:34:08 -0600

    add color and waifu states

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+
+import { Drawer } from '../components/drawer/drawer';
+import { Timer } from '../components/timer/timer';
+import styles from '../styles/Home.module.css';
+
+import { useState } from 'react';
 
 export default function Home() {
+  const [bgColor, setBgColor] = useState('#1d1d1d');
+  const [fontColor, setFontColor] = useState('#fff');
+  const [waifu, setWaifu] = useState('/rem.png');
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,45 +20,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className={styles.main} style={{ color: fontColor }}>
+        <Drawer
+          bgColor={bgColor}
+          setBgColor={setBgColor}
+          setFontColor={setFontColor}
+          setWaifu={setWaifu}
+        />
+        <Timer bgColor={bgColor} waifu={waifu} />
+        <img src={waifu} className={styles.waifu_pic} />
       </main>
 
       <footer className={styles.footer}>
@@ -65,5 +44,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
